2827f9d407e958295c62eb97ed44c8b2e68f6c92
Helen-122 <helene.mazourine@gmail.com>
2021-04-11 20:08:53 +0200

    js part2 ex6

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeImg1(){
+  var image1 = document.getElementById('img1').src = "/Users/Helene/Desktop/partie-2-JS/img/image1_2.jpg"
+}
+function changeImg2(){
+  var image2 = document.getElementById('img2').src = "/Users/Helene/Desktop/partie-2-JS/img/image2.jpg"
+}
+function changeImg3(){
+  var image3 = document.getElementById('img3').src = "/Users/Helene/Desktop/partie-2-JS/img/image3_2.jpg"
+}
+function changeImg4(){
+  var image4 = document.getElementById('img4').src = "/Users/Helene/Desktop/partie-2-JS/img/image4_2.jpg"
+}
